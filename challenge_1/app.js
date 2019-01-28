feb5bdef8aeae2c2b6f9e769f3daef503520a0e7
John Castberg <jpcastberg@gmail.com>
2019-01-28 14:27:14 -0800

    implement basic game functionality

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,11 @@
-const board = [[,,],[,,],[,,]];
+var board = [['','',''],['','',''],['','','']];
 
 var currentPlayer = 'X';
 var winningBoard = false;
+
+///////////////////////////
+// WIN CHECKERS
+///////////////////////////
 
 const horizontalWinChecker = (row) => {
   var pieces = 0;
@@ -28,7 +32,6 @@ const verticalWinChecker = (col) => {
 }
 
 const diagonalWinChecker = () => {
-  winningBoard = false; // REMOVE AFTER TESTING
   var pieces = 0;
   //MAJOR DIAGONAL
   for (var i = 0; i < board.length; i++) {
@@ -52,10 +55,70 @@ const diagonalWinChecker = () => {
     }
   }
 }
-var board = [['O','O','X'],['O','X','O'],['X','O','O']];
 
-document.getElementsByTagName('body').addEventListener('click', (event) => {
-  //click handler
+const checkForWin = (row, col) => {
+  
+  horizontalWinChecker(row);
+  verticalWinChecker(col);
+  diagonalWinChecker();
+  
+  if (winningBoard) {
+    setTimeout(() => {
+      alert(currentPlayer + ' wins!');
+      resetBoard();
+    }, 100);
+  }
+  
+}
+
+const checkValidityOfMoveAndPlacePiece = (location) => {
+  var row = parseInt(location[0]);
+  var col = parseInt(location[1]);
+  
+  if (board[row][col] === '') {
+    board[row][col] = currentPlayer;
+    checkForWin(row, col);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const resetBoard = () => {
+  winningBoard = false;
+  board = [['','',''],['','',''],['','','']];
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      var idName = i.toString() + j.toString();
+      document.getElementById(idName).innerHTML = '';
+    }
+  }
+}
+
+//var board = [['O','O','X'],['O','X','O'],['X','O','O']];
+
+///////////////////////////
+// DOM MANIPULATION
+///////////////////////////
+
+
+document.addEventListener('click', (event) => {
+  
+  if (event.target.className === 'square') {
+    var location = event.target.id;
+    if (checkValidityOfMoveAndPlacePiece(location)) {
+      event.target.innerHTML = currentPlayer;
+      if(!winningBoard) {
+        currentPlayer === 'X' ? currentPlayer = 'O' : currentPlayer = 'X';
+        document.getElementById('your-move').innerHTML = 'It\'s your move, ' + currentPlayer + '!';
+      }
+    }
+  }
+  
+  // if (event.target.className = 'reset') {
+  //   resetBoard();
+  // }
+  
 });
 
 
