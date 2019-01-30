4869579e07b907c389767b1dc34f13b27fd0dc41
John Castberg <jpcastberg@gmail.com>
2019-01-29 17:23:41 -0800

    complete json to csv converter

---
+++ challenge_2/jsonConverter.js
@@ -0,0 +1,69 @@
+
+// The server must flatten the JSON hierarchy, mapping each item/object in the JSON
+// to a single line of CSV report (see included sample output),
+// where the keys of the JSON objects will be the columns of the CSV report.
+//
+// You may assume the JSON data has a regular structure and hierarchy
+// (see included sample file). In other words, all sibling records at a
+// particular level of the hierarchy will have the same set of properties,
+// but child objects might not contain the same properties.
+// In all cases, every property you encounter must be present in the final CSV output.
+//
+// You may also assume that child records in the JSON
+// will always be in a property called `children`.
+
+const convertJSONToCSV = (JSON) => {
+  let CSVOutput = '';
+  let headers = {};
+  let headersArr;
+
+  // GET ALL HEADERS (KEYS) IN JSON
+  const generateHeaders = () => {
+    const getHeaders = (currentObject) => {
+      let keys = Object.keys(currentObject);
+      keys.forEach((key, idx) => {
+        // Ignore 'children' key
+        if (key === 'children') return;
+        // Add key to 'headers' object if it has not already been added
+        if (!headers[key]) {
+          headers[key] = key;
+          CSVOutput += `${key},`;
+        }
+      });
+      if (currentObject.children.length > 0) {
+        currentObject.children.forEach((childObject) => {
+          getHeaders(childObject);
+        });
+      }
+    };
+    getHeaders(JSON);
+    headersArr = Object.keys(headers);
+    CSVOutput = `${CSVOutput.substring(0, CSVOutput.length - 1)}\n`;
+  };
+  const generateValueLines = () => {
+    const getValueLines = (currentObject) => {
+      let CSVLine = '';
+      headersArr.forEach((header, idx) => {
+        if (currentObject[header]) {
+          CSVLine += currentObject[header];
+        }
+        if (idx < headersArr.length - 1) {
+          CSVLine += ',';
+        } else {
+          CSVLine += '\n';
+        }
+      });
+      CSVOutput += CSVLine;
+      if (currentObject.children.length > 0) {
+        currentObject.children.forEach((child) => {
+          getValueLines(child);
+        });
+      }
+    };
+    getValueLines(JSON);
+    CSVOutput = CSVOutput.trim();
+  };
+  generateHeaders();
+  generateValueLines();
+  return CSVOutput;
+};
