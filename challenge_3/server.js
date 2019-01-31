90182acccc4597cffacc84ba92f7fd83c6e9ee8a
John Castberg <jpcastberg@gmail.com>
2019-01-30 17:12:31 -0800

    render basic react dom, serve from localhost

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
